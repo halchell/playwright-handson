432ea64e102dedd6978c9807d9f9df8461d53ee0
halchell <halchell1423@gmail.com>
2025-11-29 15:03:46 +0900

    test: implement navigation test to form page

---
+++ playwright-handson/tests/form.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('フォーム画面への遷移', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('link', { name: '入力フォーム' }).click();
+  await expect(page.getByRole('heading', { name: '入力フォーム' })).toBeVisible();
+})
